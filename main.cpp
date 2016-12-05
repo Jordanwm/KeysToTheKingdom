9729abd74d929a77646dac14ddf7bf8c9d1f8cd6
Taylor McClure <taylor.mcclure@radixmeta.com>
2016-12-05 13:53:57 -0700

    Added car

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
  *  File: main.cpp
  *
  *  Author: Jordan Moser
- *      Taylor McClure 
+ *      Taylor McClure
  *      Claire Collier - Fall 2015
  *
  */
 
  /*
  *
- * TODO: 
+ * TODO:
  * 1) todos in Map file.
  * 2) Add Hero to road.
  * 3) Control hero with mouse movement
@@ -58,6 +58,187 @@ bool gKeysPressed[256] = {false};
 Camera* gCamera = NULL;
 Map* gMap = NULL;
 
+float wheel_rotation = 0;
+float prop_rotation = 0;
+float rotate_angle = 0;
+
+void drawProp() {
+	glColor3ub(0, 255, 0);
+	glPushMatrix(); {
+		glScalef(0.2, 2, 0.2);
+		glutSolidCube(0.5);
+	} glPopMatrix();
+	glColor3ub(255,255, 0);
+	glPushMatrix(); {
+		glScalef(2, 0.2, 0.2);
+		glutSolidCube(0.5);
+	} glPopMatrix();
+	glColor3ub( 0, 0, 0);
+	glPushMatrix(); {
+		glutSolidSphere(0.1, 15, 15);
+	} glPopMatrix();
+}
+
+void drawWheel() {
+	GLUquadric *cylinder;
+	GLUquadric *disk;
+
+	//Draw Tire
+	glColor3ub(50, 50, 50);
+	glPushMatrix(); {
+		cylinder = gluNewQuadric();
+		gluCylinder(cylinder, 0.5f, 0.5f, 0.25f, 15, 15);
+	} glPopMatrix();
+
+	glPushMatrix(); {
+		disk = gluNewQuadric();
+		gluDisk(disk, 0, 0.5f, 15, 15);
+	} glPopMatrix();
+
+	glPushMatrix(); {
+		glTranslatef(0, 0, 0.25);
+		disk = gluNewQuadric();
+		gluDisk(disk, 0, 0.5f, 15, 15);
+	} glPopMatrix();
+
+
+	//Draw rim
+	glColor3f(255, 255, 255);
+	glPushMatrix(); {
+		glTranslatef(0, 0, -0.01f);
+		disk = gluNewQuadric();
+		gluDisk(disk, 0, 0.4f, 6, 6);
+	} glPopMatrix();
+
+}
+
+void drawBody() {
+	GLUquadric *cylinder;
+	GLUquadric *disk;
+
+	glColor3ub(255,25, 25);
+	//Main body
+	glPushMatrix(); {
+		glScalef(1.2, 1, 2.5);
+		//glutSolidCube(1);
+		glTranslatef(0, 0, -0.5);
+		cylinder = gluNewQuadric();
+		gluCylinder(cylinder, 0.5f, 0.5f, 1.0f, 15, 15);
+	} glPopMatrix();
+	//Front sphere
+	glPushMatrix(); {
+		glScalef(1.2, 1, 2);
+		glTranslatef(0, 0, 1.0);
+		//glutSolidCube(1);
+		glTranslatef(0, 0, -0.5);
+		glutSolidSphere(0.5, 15,15);
+	} glPopMatrix();
+	//Rear sphere
+	glPushMatrix(); {
+		glScalef(1.2, 1, 1);
+		glTranslatef(0, 0, -0.7);
+		//glutSolidCube(1);
+		glTranslatef(0, 0, -0.5);
+		glutSolidSphere(0.5, 15,15);
+	} glPopMatrix();
+	//Rear Pop shaft
+	glColor3ub(20,20,20);
+	glPushMatrix(); {
+		glScalef(1.2, 1, -2.5);
+		//glutSolidCube(1);
+		glTranslatef(0, 0, 0);
+		cylinder = gluNewQuadric();
+		gluCylinder(cylinder, 0.1f, 0.1f, 0.8f, 15, 15);
+	} glPopMatrix();
+
+
+	//Main upper cylinder
+	glColor3ub(100, 100, 100);
+	glPushMatrix(); {
+		cylinder = gluNewQuadric();
+		glTranslatef(0, 0.25, -1);
+		gluCylinder(cylinder, 0.5f, 0.5f, 1.0f, 15, 15);
+	} glPopMatrix();
+	//Front upper cylinder
+	glPushMatrix(); {
+		cylinder = gluNewQuadric();
+		glTranslatef(0, 0.25, 0);
+		gluCylinder(cylinder, 0.5f, 0.0f, 1.8f, 15, 15);
+	} glPopMatrix();
+	//Rear upper cylinder
+	glPushMatrix(); {
+		cylinder = gluNewQuadric();
+		glTranslatef(0, 0.25, -1.5);
+		gluCylinder(cylinder, 0.0f, 0.5f, 0.5f, 15, 15);
+	} glPopMatrix();
+
+	glPushMatrix(); {
+		glTranslatef(0,0, -2);
+		glRotatef(prop_rotation, 0, 0, 1);
+		drawProp();
+	} glPopMatrix();
+}
+
+void drawCar() {
+	//Draw body
+	glPushMatrix(); {
+		drawBody();
+	} glPopMatrix();
+
+	//Draw front axle
+	glColor3f(0,0, 255);
+	glPushMatrix(); {
+		glRotatef(90, 0, 1, 0);
+		glTranslatef(-1, 0, -1);
+		GLUquadric *quadratic;
+		quadratic = gluNewQuadric();
+		gluCylinder(quadratic, 0.1f, 0.1f, 2.0f, 32, 32);
+	} glPopMatrix();
+
+	//Front left wheel
+	glPushMatrix(); {
+		glTranslatef(1, 0, 1);
+		glRotatef(270, 0, 1, 0);
+		glRotatef(-1 * wheel_rotation, 0, 0, 1);
+		drawWheel();
+	} glPopMatrix();
+
+	//Front right wheel
+	glPushMatrix(); {
+		glTranslatef(-1, 0, 1);
+		glRotatef(90, 0, 1, 0);
+		glRotatef(wheel_rotation, 0, 0, 1);
+		drawWheel();
+	} glPopMatrix();
+
+	//Draw rear axle
+	glColor3f(0,0, 255);
+	glPushMatrix(); {
+		glRotatef(90, 0, 1, 0);
+		glTranslatef(1, 0, -1);
+		GLUquadric *quadratic;
+		quadratic = gluNewQuadric();
+		gluCylinder(quadratic, 0.1f, 0.1f, 2.0f, 32, 32);
+	} glPopMatrix();
+
+	//Rear left wheel
+	glPushMatrix(); {
+		glTranslatef(1, 0, -1);
+		glRotatef(270, 0, 1, 0);
+		glRotatef(-1 * wheel_rotation, 0, 0, 1);
+		drawWheel();
+	} glPopMatrix();
+
+	//Rear right wheel
+	glPushMatrix(); {
+		glTranslatef(-1, 0, -1);
+		glRotatef(90, 0, 1, 0);
+		glRotatef(wheel_rotation, 0, 0, 1);
+		drawWheel();
+	} glPopMatrix();
+}
+
+
 double getRand() {
     return rand() / (double)RAND_MAX;
 }
@@ -88,8 +269,8 @@ void initScene()  {
     glLightfv( GL_LIGHT0, GL_AMBIENT, ambientCol );
     glEnable( GL_LIGHTING );
     glEnable( GL_LIGHT0 );
-    
-    // tell OpenGL not to use the material system; just use whatever we 
+
+    // tell OpenGL not to use the material system; just use whatever we
     // pass with glColor*()
     glEnable( GL_COLOR_MATERIAL );
     glColorMaterial( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE );
@@ -117,7 +298,14 @@ void renderScene(void)  {
 
     if (gMap)
         gMap->Draw();
-    
+
+    glPushMatrix(); {
+		glTranslatef(getLocation().getX(), getLocation().getY(), getLocation().getZ());
+        glScalef(0.2, 0.2, 0.2);
+		glRotatef(rotate_angle, 0, 1, 0);
+		drawCar();
+	} glPopMatrix();
+
     //push the back buffer to the screen
     glutSwapBuffers();
 }
@@ -138,7 +326,7 @@ void normalKeysUp(unsigned char key, int x, int y) {
 void mouseCallback(int button, int state, int x, int y) {
     if(button == GLUT_LEFT_BUTTON)
         gLeftMouseButton = state;
-        
+
     gMouseX = x;
     gMouseY = y;
 
@@ -147,7 +335,7 @@ void mouseCallback(int button, int state, int x, int y) {
     }
     else {
         gCtrlDown = false;
-    }   
+    }
 }
 
 void mouseMotion(int x, int y) {
@@ -184,7 +372,7 @@ void updateScene(int value){
         gCamera->setLookAt(gMap->getLocation());
         gCamera->Recompute();
     }
-        
+
     glutPostRedisplay();
 
     glutTimerFunc(1000.0/60.0, updateScene, 0);
@@ -212,7 +400,7 @@ int main(int argc, char **argv) {
     initScene();
 
     //LoadTextures();
-    
+
     if (!LoadGameFile(argc, argv)) // Load config file containing map, texture names, etc.
         return(1);
 
